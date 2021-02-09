f7658a0dfbb331b8919e395e7d717fd5aecb665a
Vaibz123 <vaibhaviii0798@gmail.com>
2021-02-09 18:50:19 +0530

    Weather app

---
+++ README.md
@@ -0,0 +1,33 @@
+# Introduction 
+Weather App displays current weather of place on entering location (i.e. city)
+# Getting Started
+1.	## Installation commands ##
+-Run the following command from inside your app directory (i.e. where package.json is located) to 
+install all dependencies:
+### ->npm install ### 
+
+
+2.	## Software dependencies: ##
+    * Express
+    * ejs 
+    * request 
+    * ts-node 
+
+3.	## API references ##
+The weather data will come from the [OpenWeatherMap API](http://api.openweathermap.org/api), and just like any other API, you need to sign up for an API key in order to use it. Go ahead and sign up for an account. Once you're logged in, go to the API keys page, copy the value of the key field and store it in __apiKey__ variable in _index.ts_ file.
+
+![](img/api_snippet.JPG)
+
+#Technologies
+
+* NodeJS
+* TypeScript
+* Express
+
+# Build and Test
+npm run start
+
+#Overview 
+
+![](img/weather_app.JPG)
+
---
+++ index.ts
@@ -0,0 +1,72 @@
+//const router=require("./router"); 
+const express = require("express");
+
+//const bodyParser = require('body-parser');
+const app = express();
+const port = 9999;
+const request=require('request');
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+let ejs=require('ejs');
+let city:string="Thane";
+let apiKey:string="8163d4bdd3efb940773f3b9c796fb070";
+let url_api:string=`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+
+app.set('view engine','ejs');
+
+app.get("/", (req, res) => {
+    
+    res.render("index",{icon_url:"",temp_min:"",temp_max:"",city:"",country:"",description:"",temp:"",pressure:"",humidity:"",wind_speed:"",weather:""});
+             
+    });
+
+app.post('/weather', function (req, res) {
+    //console.log('Got body:', req.body);
+    //res.sendStatus(200);
+    let city=req.body.city;
+    console.log("City",city);
+    url_api=`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    request(url_api,function(err,response,body){
+    
+        if(err){
+            console.log('error',err);
+            res.render("index",{icon_url:"",temp_min:"",temp_max:"",city:"",country:"",description:"",temp:"",pressure:"",humidity:"",wind_speed:"",weather:""});
+    
+            }
+        else{
+            let weather:JSON =JSON.parse(body);
+            let city:string=req.body.city;
+            let country:string=weather["sys"].country;
+            let temp:number =weather["main"].temp;
+            let pressure:number=weather["main"].pressure;
+            let humidity:number=weather["main"].humidity;
+            let temp_min:number=weather["main"].temp_min;
+            let temp_max:number=weather["main"].temp_max;
+            
+            let wind_speed:number=weather["wind"].speed; 
+            let description:string=weather["weather"][0].description;
+            let icon:string=weather["weather"][0].icon;
+            let icon_url:string=`http://openweathermap.org/img/wn/${icon}@2x.png`;
+            console.log("Icon name",icon);
+            //console.log("Weather[0]",weather["weather[0]"]);
+            console.log("Description",description);
+            console.log('Temperature',temp);
+            console.log('Pressure',pressure);
+            console.log('Humidity',humidity);
+            console.log('Wind Speed',wind_speed);
+            res.render("index",{icon_url:icon_url,temp_min:temp_min,temp_max:temp_max,city:city,country:country,description:description,weather:weather,temp:temp,pressure:pressure,humidity:humidity,wind_speed:wind_speed});
+
+    
+        }
+        });
+       // res.sendStatus(200);
+  });
+
+  app.listen(port, function() {
+    console.log(`Server listening on port ${port}`);
+  });
+  
